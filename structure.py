0d4b0c6ea8fd1a1507e98ee1f2211e2ca289a3bf
hazen8ter <124236464+hazen8ter@users.noreply.github.com>
2025-07-09 15:17:12 -0400

    Create structure.py

---
+++ structure.py
@@ -0,0 +1,33 @@
+#Acquire first name, last name, ID, number of dependents, and hours worked
+first_name = input ("What is your first name?")
+last_name = input ("What is your last name?")
+employee_ID = int(input ("What is your employee ID?"))
+hours = int(input ("How many hours did you work?"))
+hourly_rate = int(input("What is your hourly"))
+
+'''
+After get the name and matching data that will pull things like rate from a list
+we can then utilize the information we got to show the result of their pay.
+
+Hardest thing will be matching the employee but I think the easier way is only 
+matching the ID to the hourly rate so no way to mix-up capatilization. 
+
+'''
+#Reasign variables from a list/array from known data about rate of pay
+
+
+#function of gross pay will go here
+
+
+
+
+#Calculating taxes will go here such as State Tax and Federal subtracting from gross
+#Pre-tax is stuff like taxes due
+dependents = int(input ("How many dependents?"))
+stateTax = .056
+federalTax = .079
+
+#Post-tax is stuff after taxes that gets calculated for investments or personal contributions
+
+
+#Results
